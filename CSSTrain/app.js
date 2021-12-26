2bf98b4a41573397eae82fc0a2ca5cdc9d11b9b5
TomislavMilivojevic <tomimilimilivojevic@gmail.com>
2021-12-26 16:46:35 +0200

    calc3

--- CSSTrain/app.js
+++ CSSTrain/app.js
@@ -13,59 +13,77 @@ let numarUnu;
 let numarDoi;
 let rezultat;
 let operator;
+let operatorVechi;
 
 const handleButoane = (e) => {
   ecranUnu.innerText += e.target.innerText;
 };
 
+const executaActiune = (semnVechi) => {
+  if (semnVechi === "+") {
+    rezultat = numarUnu + numarDoi;
+    console.log(rezultat);
+    ecranDoi.innerText = rezultat;
+
+    ecranUnu.innerText = "";
+  } else if (semnVechi === "/") {
+    rezultat = numarUnu / numarDoi;
+    console.log(rezultat);
+    ecranDoi.innerText = rezultat;
+
+    ecranUnu.innerText = "";
+  } else if (semnVechi === "-") {
+    rezultat = numarUnu - numarDoi;
+    console.log(rezultat);
+    ecranDoi.innerText = rezultat;
+
+    ecranUnu.innerText = "";
+  } else if (semnVechi === "*") {
+    rezultat = numarUnu * numarDoi;
+    console.log(rezultat);
+    ecranDoi.innerText = rezultat;
+
+    ecranUnu.innerText = "";
+  } else if (semnVechi === "%") {
+    rezultat = numarUnu % numarDoi;
+    console.log(rezultat);
+    ecranDoi.innerText = rezultat;
+
+    ecranUnu.innerText = "";
+  }
+};
+
 handleActiune = (e) => {
+  // retinem vechium operator, va fi folosit la functia de operatii
+
+  if (
+    ecranUnu.innerText === "" &&
+    ecranOperator.innerText != "" &&
+    ecranDoi.innerText != ""
+  ) {
+    return;
+  }
+  if (ecranOperator.innerText != "") {
+    operatorVechi = ecranOperator.innerText;
+  }
+
   ecranOperator.innerText = e.target.innerText;
   // trebuie un if check daca ecranele sunt goale pentru a nu continua daca prima accesare este un operator
 
-  let operator = ecranOperator.innerText;
+  operator = ecranOperator.innerText;
+  console.log("OperatorNou" + operator);
   if (ecranUnu.innerText != "" && ecranDoi.innerText === "") {
     numarUnu = parseInt(ecranUnu.innerText);
     ecranDoi.innerText = ecranUnu.innerText;
     ecranUnu.innerText = "";
   } else if (ecranDoi != "" && ecranUnu != "") {
+    console.log("OperatorVechi" + operatorVechi);
     numarUnu = parseInt(ecranDoi.innerText);
     // trebuie un check pentru operatorul trecut si pentru operatorul curent(momentul clickului)
-    console.log(numarUnu + " " + numarDoi);
-    if (ecranUnu.innerText != "") {
-      numarDoi = parseInt(ecranUnu.innerText);
-      ecranUnu.innerText = "";
-    }
-    if (operator === "+") {
-      rezultat = numarUnu + numarDoi;
-      console.log(rezultat);
-      ecranDoi.innerText = rezultat;
-
-      ecranUnu.innerText = "";
-    } else if (operator === "/") {
-      rezultat = numarUnu / numarDoi;
-      console.log(rezultat);
-      ecranDoi.innerText = rezultat;
-
-      ecranUnu.innerText = "";
-    } else if (operator === "-") {
-      rezultat = numarUnu - numarDoi;
-      console.log(rezultat);
-      ecranDoi.innerText = rezultat;
-
-      ecranUnu.innerText = "";
-    } else if (operator === "*") {
-      rezultat = numarUnu * numarDoi;
-      console.log(rezultat);
-      ecranDoi.innerText = rezultat;
-
-      ecranUnu.innerText = "";
-    } else if (operator === "%") {
-      rezultat = numarUnu % numarDoi;
-      console.log(rezultat);
-      ecranDoi.innerText = rezultat;
-
-      ecranUnu.innerText = "";
-    }
+    console.log("Numar unu " + numarUnu + " " + " Numar doi " + numarDoi);
+    numarDoi = parseInt(ecranUnu.innerText);
+    ecranUnu.innerText = "";
+    executaActiune(operatorVechi);
   }
 };
 
